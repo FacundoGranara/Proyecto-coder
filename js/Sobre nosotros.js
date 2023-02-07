9430d85b696e73ab6b82ca9bf94bc7d190f2651c
Facundo Granara <andoflexing@gmail.com>
2023-02-07 07:12:58 -0300

    Nuevas imagenes, icono, linkeos, variables y efectos nuevos.

---
+++ js/Sobre nosotros.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.img-container', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});
+
+sr.reveal('.texto', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+});
+
+sr.reveal('.logo', {
+    duration: 4000,
+    origin: 'right',
+    distance: '-200px'
+});
---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.logo', {
+        duration: 2000,
+        origin: 'botom',
+        distance: '-100px'
+    });
+
+    sr.reveal('#hero', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
